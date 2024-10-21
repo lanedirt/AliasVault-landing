552d279e16070785f507eb67218cf694a82e8521
Leendert de Borst <ldeborst@xivisoft.com>
2024-10-21 23:11:55 +0200

    Update URLs

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -176,7 +176,7 @@ const Header = () => {
                     </div>
                 </Link>
                 <Link
-                  href="https://main.aliasvault.net/user/register"
+                  href="https://app.aliasvault.net/user/setup"
                   className="ease-in-up shadow-btn hover:shadow-btn-hover hidden rounded-sm bg-primary px-8 py-3 text-base font-medium text-white transition duration-300 hover:bg-opacity-90 md:block md:px-9 lg:px-6 xl:px-9"
                 >
                   Open app
