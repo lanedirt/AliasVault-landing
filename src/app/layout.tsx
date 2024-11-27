eae6a9b70fcc5979749c0b0659fe5df91a86bb3b
Leendert de Borst <ldeborst@xivisoft.com>
2024-11-27 17:53:41 +0100

    Add open graph tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,44 @@
-"use client";
-
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
+import { Providers } from "./providers";
 import { Inter } from "next/font/google";
 import "../styles/index.css";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  metadataBase: new URL('https://aliasvault.net'),
+  title: {
+    default: 'AliasVault',
+    template: '%s | AliasVault'
+  },
+  description: 'AliasVault is an open-source end-to-end encrypted password and alias manager.',
+  openGraph: {
+    type: 'website',
+    url: 'https://aliasvault.net',
+    siteName: 'AliasVault',
+    title: 'AliasVault',
+    description: 'AliasVault is an open-source end-to-end encrypted password and alias manager.',
+    images: [
+      {
+        url: 'images/og/og-header.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'AliasVault'
+      }
+    ]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'AliasVault',
+    description: 'AliasVault is an open-source end-to-end encrypted password and alias manager.',
+    site: 'aliasvault.net',
+    creator: '@AliasVault',
+    images: ['images/og/og-header.jpg']
+  }
+};
 
 export default function RootLayout({
   children,
@@ -34,5 +66,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-import { Providers } from "./providers";
