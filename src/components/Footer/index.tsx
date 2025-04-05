979b1a3347e169932e46b564f19e4e951e618ff1
Leendert de Borst <ldeborst@xivisoft.com>
2025-04-05 20:05:06 +0200

    Update footer links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -122,10 +122,25 @@ const Footer = () => {
                   </li>
                   <li>
                     <Link
-                      href="/docs"
+                      href="https://docs.aliasvault.net"
+                      target="_blank"
                       className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
                     >
-                      Docs
+                      Documentation
+                      <svg
+                        className="ml-1 inline-block h-4 w-4"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                        />
+                      </svg>
                     </Link>
                   </li>
                   <li>
@@ -135,6 +150,20 @@ const Footer = () => {
                       className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
                     >
                       Roadmap
+                      <svg
+                        className="ml-1 inline-block h-4 w-4"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                        />
+                      </svg>
                     </Link>
                   </li>
                 </ul>
