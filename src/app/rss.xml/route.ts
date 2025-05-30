981b1399ea98ee1822e54111135fe815977239bb
Leendert de Borst <ldeborst@xivisoft.com>
2025-05-31 00:08:51 +0200

    Use dynamic base URL in RSS feed

--- src/app/rss.xml/route.ts
+++ src/app/rss.xml/route.ts
@@ -4,6 +4,7 @@ import { remark } from 'remark';
 import html from 'remark-html';
 
 export async function GET() {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://www.aliasvault.net';
   const posts = getAllBlogAndNewsPosts();
 
   // Process posts to clean up content
@@ -32,16 +33,16 @@ export async function GET() {
     <rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom">
       <channel>
         <title>AliasVault Blog</title>
-        <link>https://aliasvault.com</link>
+        <link>${baseUrl}</link>
         <description>Latest blog posts and news from AliasVault</description>
         <language>en-US</language>
         <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
-        <atom:link href="https://aliasvault.com/rss.xml" rel="self" type="application/rss+xml" />
+        <atom:link href="${baseUrl}/rss.xml" rel="self" type="application/rss+xml" />
         ${processedPosts.map((post) => `
           <item>
             <title><![CDATA[${post.title}]]></title>
-            <link>https://aliasvault.com/${post.type}/${post.slug}</link>
-            <guid>https://aliasvault.com/${post.type}/${post.slug}</guid>
+            <link>${baseUrl}/${post.type}/${post.slug}</link>
+            <guid>${baseUrl}/${post.type}/${post.slug}</guid>
             <pubDate>${new Date(post.date).toUTCString()}</pubDate>
             <description><![CDATA[${post.content}]]></description>
             <category><![CDATA[${post.tags.join(', ')}]]></category>
