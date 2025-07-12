6773ce23df8620b5e6840c8e4d4764836a79ebf0
Leendert de Borst <ldeborst@xivisoft.com>
2025-07-12 15:01:36 +0200

    Localize blog layouts

--- src/app/[locale]/error/page.tsx
+++ src/app/[locale]/error/page.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
-
 import { Metadata } from "next";
+import { useTranslations } from "next-intl";
 
 export const metadata: Metadata = {
-  title: "Error Page | Free Next.js Template for Startup and SaaS",
-  description: "This is Error Page for Startup Nextjs Template",
-  // other metadata
+  title: "Error Page | AliasVault",
+  description: "The page you requested could not be found.",
 };
 
 const ErrorPage = () => {
+  const t = useTranslations();
   return (
     <>
       <section className="relative z-10 pb-16 pt-36 md:pb-20 lg:pb-28 lg:pt-[180px]">
@@ -142,17 +142,16 @@ const ErrorPage = () => {
                   </svg>
                 </div>
                 <h3 className="mb-4 text-3xl font-bold text-black dark:text-white sm:text-4xl">
-                  Sorry, the page can’t be found
+                  {t('error.heading')}
                 </h3>
                 <p className="mb-10 text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
-                  The page you were looking for appears to have been moved,
-                  deleted or does not exist.
+                  {t('error.message')}
                 </p>
                 <Link
                   href="/"
                   className="px-8 py-3 text-base font-bold text-white duration-300 rounded-md bg-primary shadow-signUp hover:bg-white hover:text-primary md:px-9 lg:px-8 xl:px-9"
                 >
-                  Back to Homepage
+                  {t('error.backToHome')}
                 </Link>
               </div>
             </div>
