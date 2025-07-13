35c39b5c826c45e6a17d856b64aebbfec4dba1ad
Leendert de Borst <ldeborst@xivisoft.com>
2025-07-13 17:25:29 +0200

    Localize mission page and misc

--- src/app/[locale]/mission/page.tsx
+++ src/app/[locale]/mission/page.tsx
@@ -2,27 +2,30 @@ import Breadcrumb from "@/components/Common/Breadcrumb";
 import SectionSpacing from "@/components/Common/SectionSpacing";
 import MissionSection from "@/components/Mission/MissionSection";
 import Roadmap from "@/components/Mission/Roadmap";
-
+import { getTranslations } from "next-intl/server";
 import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Mission",
-  description: "Read more about our mission and vision.",
-   // other metadata
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const t = await getTranslations();
 
-const MissionPage = () => {
+  return {
+    title: t('mission.metadata.title'),
+    description: t('mission.metadata.description'),
+  };
+}
+
+export default async function MissionPage() {
+  const t = await getTranslations();
+
   return (
     <>
       <Breadcrumb
-        pageName="Our Mission"
-        description="Read more about the mission and vision for AliasVault."
+        pageName={t('mission.breadcrumb.title')}
+        description={t('mission.breadcrumb.description')}
       />
       <MissionSection />
       <SectionSpacing withBorder className="pt-16 md:pt-20" />
       <Roadmap />
     </>
   );
-};
-
-export default MissionPage;
+}
