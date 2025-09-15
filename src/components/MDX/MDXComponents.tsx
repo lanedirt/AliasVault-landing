3956eacee010e66f36e5ec55144a420fb0fde779
Leendert de Borst <ldeborst@xivisoft.com>
2025-09-15 18:33:12 +0200

    Update MDXComponents.tsx

--- src/components/MDX/MDXComponents.tsx
+++ src/components/MDX/MDXComponents.tsx
@@ -24,25 +24,25 @@ export const components = {
   ),
   h1: (props: ComponentProps<'h1'>) => (
     <h1
-      className="mb-8 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight"
+      className="mb-12 mt-8 text-2xl font-bold leading-tight text-black dark:text-white sm:text-3xl sm:leading-tight"
       {...props}
     />
   ),
   h2: (props: ComponentProps<'h2'>) => (
     <h2
-      className="mb-6 text-2xl font-bold leading-tight text-black dark:text-white sm:text-3xl sm:leading-tight"
+      className="mb-8 mt-12 text-xl font-bold leading-tight text-black dark:text-white sm:text-2xl sm:leading-tight"
       {...props}
     />
   ),
   h3: (props: ComponentProps<'h3'>) => (
     <h3
-      className="mb-4 text-xl font-bold leading-tight text-black dark:text-white sm:text-2xl sm:leading-tight"
+      className="mb-6 mt-8 text-lg font-medium leading-tight text-black dark:text-white sm:text-xl sm:leading-tight"
       {...props}
     />
   ),
   p: (props: ComponentProps<'p'>) => (
     <p
-      className="mb-6 text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed lg:text-base lg:leading-relaxed xl:text-lg xl:leading-relaxed"
+      className="mb-6 text-base leading-relaxed text-body-color md:text-lg md:leading-relaxed"
       {...props}
     />
   ),
@@ -60,12 +60,12 @@ export const components = {
   ),
   li: (props: ComponentProps<'li'>) => (
     <li
-      className="mb-2 text-base font-medium text-body-color"
+      className="mb-2 text-base leading-relaxed text-body-color md:text-lg md:leading-relaxed"
       {...props}
     />
   ),
   blockquote: (props: ComponentProps<'blockquote'>) => (
-    <div className="relative z-10 mb-10 overflow-hidden rounded-md bg-primary bg-opacity-10 p-8 md:p-9 lg:p-8 xl:p-9">
+    <div className="relative z-10 mb-6 overflow-hidden rounded-md bg-primary bg-opacity-10 p-4 md:p-5">
       <blockquote className="text-center text-base font-medium italic text-body-color" {...props} />
     </div>
   ),
